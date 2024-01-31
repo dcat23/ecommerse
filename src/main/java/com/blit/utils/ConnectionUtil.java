8c5f7e04bb61fce6441f5acea17f8af8f20661bd
Devin Catuns <devincatuns1@gmail.com>
2024-01-31 00:00:21 -0500

    create: database ConnectionUtil

---
+++ src/main/java/com/blit/utils/ConnectionUtil.java
@@ -0,0 +1,31 @@
+package com.blit.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static Connection conn;
+
+    public static Connection getConnection() throws SQLException {
+        if (conn != null && !conn.isClosed()) {
+            return conn;
+        } else {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // update once connecting to live databasse
+//            String url = "demos-database.cdomywmkyglo.us-east-1.rds.amazonaws.com:3306";
+            final String url = "http://locahost:3308/exchange";
+            final String username = "admin";
+            final String password = "password";
+
+            conn = DriverManager.getConnection(url, username, password);
+        }
+
+        return conn;
+    }
+}
